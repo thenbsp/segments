94de7eea5c2638a7390ea079b49ae517ab498f37
thenbsp <thenbsp@gmail.com>
2015-11-12 15:50:38 +0800

    Create SymfonyAuthenticationService.php

---
+++ SymfonyAuthenticationService.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle\Service;
+
+use AppBundle\Entity\User;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Http\SecurityEvents;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class Authentication
+{
+    /**
+     * Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface
+     */
+    protected $tokenStorage;
+
+    /**
+     * Symfony\Component\EventDispatcher\EventDispatcherInterface
+     */
+    protected $eventDispatcher;
+
+    /**
+     * Symfony\Component\HttpFoundation\Request
+     */
+    protected $request;
+
+    /**
+     * 构造方法
+     */
+    public function __construct(
+        TokenStorageInterface $tokenStorage,
+        EventDispatcherInterface $eventDispatcher,
+        RequestStack $requestStack)
+    {
+        $this->tokenStorage     = $tokenStorage;
+        $this->eventDispatcher  = $eventDispatcher;
+        $this->request          = $requestStack->getCurrentRequest();
+    }
+
+    /**
+     * 认证用户
+     */
+    public function authenticate(User $user, $providerKey = 'app')
+    {
+        $authenticationToken = new UsernamePasswordToken($user, null, $providerKey, $user->getRoles());
+        $authenticationEvent = new InteractiveLoginEvent($this->request, $authenticationToken);
+
+        $this->tokenStorage->setToken($authenticationToken);
+        $this->eventDispatcher->dispatch(SecurityEvents::INTERACTIVE_LOGIN, $authenticationEvent);
+    }
+
+    /**
+     * 取消认证
+     */
+    public function unauthenticate($invalidateSession = true)
+    {
+        $this->tokenStorage->setToken();
+
+        if( $invalidateSession ) {
+            $session = $this->request->getSession();
+            $session->invalidate();
+        }
+    }
+}
