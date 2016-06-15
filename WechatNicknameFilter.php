dd96a5f49f9f516aea3c9b47ea1a1305cf0b5126
thenbsp <thenbsp@gmail.com>
2016-06-15 10:08:57 +0800

    Update WechatNicknameFilter.php

--- WechatNicknameFilter.php
+++ WechatNicknameFilter.php
@@ -3,20 +3,18 @@
 // 只负责过滤表情，不包括 HTML 符号
 public function setNickname($nickname)
 {
-    // Match Emoticons
-    $nickname = preg_replace('/[\x{1F600}-\x{1F64F}]/u', '', $nickname);
+    $pattern = array(
+        '/\xEE[\x80-\xBF][\x80-\xBF]/',
+        '/\xEF[\x81-\x83][\x80-\xBF]/',
+        '/[\x{1F600}-\x{1F64F}]/u',
+        '/[\x{1F300}-\x{1F5FF}]/u',
+        '/[\x{1F680}-\x{1F6FF}]/u',
+        '/[\x{2600}-\x{26FF}]/u',
+        '/[\x{2700}-\x{27BF}]/u',
+        '/[\x{20E3}]/u'
+    );
 
-    // Match Miscellaneous Symbols and Pictographs
-    $nickname = preg_replace('/[\x{1F300}-\x{1F5FF}]/u', '', $nickname);
-
-    // Match Transport And Map Symbols
-    $nickname = preg_replace('/[\x{1F680}-\x{1F6FF}]/u', '', $nickname);
-
-    // Match Miscellaneous Symbols
-    $nickname = preg_replace('/[\x{2600}-\x{26FF}]/u', '', $nickname);
-
-    // Match Dingbats
-    $nickname = preg_replace('/[\x{2700}-\x{27BF}]/u', '', $nickname);
+    $nickname = preg_replace($pattern, '', $nickname);
 
     return trim($nickname);
 }
